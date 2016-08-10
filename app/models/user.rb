a6baa16ea0a1bf3bf719dff338321a6bff235a6c
Gardo Roman & Kyrene Jackson <pair+gardoroman.kyrene-jackson@devbootcamp.com>
2016-08-10 15:18:56 -0500

    Test user mdoel. Add foreign key to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ require 'bcrypt'
 class User < ActiveRecord::Base
   include BCrypt
 
-  has_many :questions
+  has_many :questions, foreign_key: :author_id
   has_many :answers
   has_many :comments
   has_many :votes
