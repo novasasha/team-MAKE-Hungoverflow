edcb724e85fbde338b6afb15f4234549a2ead2bf
Michelle Kelly & Aleksandra (Sasha) Ballard-Aktuganova <pair+kellymp.sasha-a@devbootcamp.com>
2016-08-10 16:14:17 -0500

    Create login/logout functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,1 +1,18 @@
+get '/users/new' do
+  erb :"/users/new"
+end
 
+post '/users' do
+  @user = User.new(params["user"])
+  if @user.save
+    redirect "/"
+  else
+    @errors = @user.errors.full_messages
+    erb :"/users/new"
+  end
+end
+
+get '/users/:id' do
+  @user = User.find_by(id: params[:id])
+  erb :"/users/show"
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,20 +3,22 @@ require 'bcrypt'
 class User < ActiveRecord::Base
   include BCrypt
 
-  has_many :questions
+  has_many :questions, foreign_key: :author_id
   has_many :answers
   has_many :comments
   has_many :votes
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
+
+  validate :validate_password
 
   def password
     @password ||= Password.new(password_hash)
   end
 
   def password=(new_password)
+    @raw_password = new_password
     @password = Password.create(new_password)
     self.password_hash = @password
   end
@@ -24,4 +26,17 @@ class User < ActiveRecord::Base
   def authenticate(password)
     self.password == password
   end
+
+  private
+  def validate_password
+    if @raw_password.length == 0
+      errors.add(:password, "is required")
+    elsif @raw_password.length < 6
+      errors.add(:password, "must be longer than 6 characters")
+    end
+  end
 end
+
+
+
+
