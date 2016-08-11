923cf5913b733db712676df8e62c503570612be7
Michelle Kelly & Aleksandra (Sasha) Ballard-Aktuganova <pair+kellymp.sasha-a@devbootcamp.com>
2016-08-11 15:09:35 -0500

    Answers functionality works

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,3 +1,14 @@
 post '/questions/:id/answers' do
-  "Hello World"
+  p params
+  @answer = Answer.new(
+          body: params['answer'][:body],
+          question_id: params['answer'][:question_id],
+          user_id: current_user.id)
+  p @answer
+  if @answer.save
+    redirect "/questions/#{@answer.question_id}"
+  else
+    erb :"/questions/#{@answer.question_id}"
+  end
 end
+
