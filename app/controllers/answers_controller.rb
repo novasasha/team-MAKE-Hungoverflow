6de67eb93b75321388440dd9401f09ba208b6a0d
Michelle Kelly & Aleksandra (Sasha) Ballard-Aktuganova <pair+kellymp.sasha-a@devbootcamp.com>
2016-08-11 15:14:35 -0500

    Add user id to answer view

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,10 +1,9 @@
 post '/questions/:id/answers' do
-  p params
   @answer = Answer.new(
           body: params['answer'][:body],
           question_id: params['answer'][:question_id],
           user_id: current_user.id)
-  p @answer
+
   if @answer.save
     redirect "/questions/#{@answer.question_id}"
   else
