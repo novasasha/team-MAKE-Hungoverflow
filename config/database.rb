81cdfff8b6599de322b34cdf494097be09b31294
gardoroman <gardoroman@gmail.com>
2016-08-10 22:26:45 -0500

    Make views to display all questions and individual question with answers and comments

--- config/database.rb
+++ config/database.rb
@@ -35,8 +35,8 @@ ActiveRecord::Base.establish_connection(
   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
   :host     => db.host,
   :port     => db.port,
-  :username => db.user,
-  :password => db.password,
+  :username => "gardo",
+  :password => "dbtestenv",
   :database => DB_NAME,
   :encoding => 'utf8'
 )
