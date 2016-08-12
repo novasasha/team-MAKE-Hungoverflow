79046592e9a7ebbfc7715d21a0f139a4a6845c27
Michelle Kelly & Aleksandra (Sasha) Ballard-Aktuganova <pair+kellymp.sasha-a@devbootcamp.com>
2016-08-11 19:18:58 -0500

    Ajaxify adding answers - in progress

--- public/js/answers.js
+++ public/js/answers.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
   $('#add-answer').on('submit', function(event) {
     event.preventDefault();
-    var
-  })
+    var answerText = $('#answer-text').val();
+      if (answerText === "") {
+        alert("Answer can not be blank")
+        return false
+      };
+
+    var url = $(this).attr('action');
+    var data = $(this).serialize();
+    $.ajax({
+      url: url,
+      data: data,
+      method: 'POST'
+    })
+    .done(function(response){
+      $('.answer-text').text(response);
+      $('#answer-text').val("")
+    });
+  });
 });
